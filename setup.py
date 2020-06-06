3f54d28b830878dede5ae75cc2f6c26583b1c14d
Peter Law <PeterJCLaw@gmail.com>
2020-06-06 23:13:30 +0100

    Declare v0.0.8

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(my_dir, 'README.md')) as f:
 
 setup(
     name='tuck',
-    version='0.0.7',
+    version='0.0.8',
     url='https://github.com/PeterJCLaw/tuck',
     project_urls={
         'Issue tracker': 'https://github.com/PeterJCLaw/tuck/issues',
