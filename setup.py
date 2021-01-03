ab9fcf6e368251b74404f9ca027c71d762edc15b
Peter Law <PeterJCLaw@gmail.com>
2021-01-03 17:01:21 +0000

    Declare 0.0.11

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (Path(__file__).parent / 'README.md').read_text()
 
 setup(
     name='tuck',
-    version='0.0.10',
+    version='0.0.11',
     url='https://github.com/PeterJCLaw/tuck',
     project_urls={
         'Issue tracker': 'https://github.com/PeterJCLaw/tuck/issues',
