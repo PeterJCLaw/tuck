fdf7c4ec7a82989a94d41ebfa5a413fe6bde1e22
Peter Law <PeterJCLaw@gmail.com>
2023-06-25 15:09:51 +0100

    Add explicit testing for applying edits

--- tests/tests_editing.py
+++ tests/tests_editing.py
@@ -1,7 +1,105 @@
+from __future__ import annotations
+
+import textwrap
 import unittest
 
 import tuck.editing
 from tuck import Range, Position
+from tuck.editing import Edit
+
+
+class TestApplyEdits(unittest.TestCase):
+    def assertEdits(
+        self,
+        edits: list[Edit],
+        content: str,
+        expected_output: str,
+        *,
+        message: str = "Bad edits",
+    ) -> None:
+        # Normalise from triple quoted strings
+        content = textwrap.dedent(content[1:])
+        expected_output = textwrap.dedent(expected_output[1:])
+
+        new_content = tuck.apply_edits(content, edits)
+
+        self.assertEqual(expected_output, new_content, message)
+
+    def test_single_insertion(self) -> None:
+        self.assertEdits(
+            [
+                Edit(Range(Position(1, 4), Position(1, 4)), '123 '),
+            ],
+            '''
+            abc def
+            ''',
+            '''
+            abc 123 def
+            ''',
+        )
+
+    def test_single_insertion_new_line(self) -> None:
+        self.assertEdits(
+            [
+                Edit(Range(Position(2, 0), Position(2, 0)), '123\n'),
+            ],
+            '''
+            abc
+            def
+            ''',
+            '''
+            abc
+            123
+            def
+            ''',
+        )
+
+    def test_single_insertion_within_line(self) -> None:
+        self.assertEdits(
+            [
+                Edit(Range(Position(2, 2), Position(2, 2)), '-123-'),
+            ],
+            '''
+            abc
+            def
+            ''',
+            '''
+            abc
+            de-123-f
+            ''',
+        )
+
+    def test_multiple_insertions_single_line(self) -> None:
+        self.assertEdits(
+            [
+                Edit(Range(Position(1, 3), Position(1, 3)), '-123-'),
+                Edit(Range(Position(1, 7), Position(1, 7)), '-456-'),
+            ],
+            '''
+            abc def
+            ''',
+            '''
+            abc-123- def-456-
+            ''',
+        )
+
+    def test_multiple_insertions_new_line(self) -> None:
+        self.assertEdits(
+            [
+                Edit(Range(Position(1, 4), Position(1, 4)), '123\n'),
+                Edit(Range(Position(2, 0), Position(2, 0)), '456\n'),
+            ],
+            '''
+            abc
+            def
+            ''',
+            '''
+            abc
+            123
+            456
+            def
+            ''',
+        )
 
 
 class TestAllAreDisjoint(unittest.TestCase):
